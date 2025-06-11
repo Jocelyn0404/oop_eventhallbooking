68b9f289815e58d92cd77a796839bb68fe4b03dc
Beatrice Ann David <148671333+bettydavid@users.noreply.github.com>
2025-06-11 12:26:13 +0800

    Update Hall.java

--- module2_bea/Hall.java
+++ module2_bea/Hall.java
@@ -1,1 +1,29 @@
 package module2_bea;
+// === 3. Hall ===
+class Hall {
+    private int hallId;
+    private String name;
+    private String location;
+    private int capacity;
+    public double pricePerHour;
+
+    public Hall(int hallId, String name, String location, int capacity, double pricePerHour) {
+        this.hallId = hallId;
+        this.name = name;
+        this.location = location;
+        this.capacity = capacity;
+        this.pricePerHour = pricePerHour;
+    }
+
+    public int getHallId() {
+        return hallId;
+    }
+
+    public double getPricePerHour() {
+        return pricePerHour;
+    }
+
+    public String toString() {
+        return hallId + ": " + name + " - " + location + " (" + capacity + " people), RM" + pricePerHour + "/hour";
+    }
+}
