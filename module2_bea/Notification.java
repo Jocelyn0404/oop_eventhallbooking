767fff67a88011fddca7f40a9d9b8a4f9d4d42c0
Beatrice Ann David <148671333+bettydavid@users.noreply.github.com>
2025-06-11 12:28:35 +0800

    Update Notification.java

--- module2_bea/Notification.java
+++ module2_bea/Notification.java
@@ -1,3 +1,13 @@
-package module2_bea;
+// === 6. Notification ===
+class Notification {
+    private int notificationId;
+    private int userId;
+    private String message;
+    private Date date;
+
+    public void sendEmail() {}
+    public void sendSMS() {}
+}
+
 
 
