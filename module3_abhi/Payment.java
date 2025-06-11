d06f1f5a01f72ddacdbeb2778d45c30665f32bb4
Abhilashinie A/P Prabaharan <148563786+Abhilashinie@users.noreply.github.com>
2025-06-11 12:25:05 +0800

    Update Payment.java

--- module3_abhi/Payment.java
+++ module3_abhi/Payment.java
@@ -1,5 +1,20 @@
 package module3_abhi;
 
 public class Payment {
-    
+   private int paymentId;
+    private int bookingId;
+    private double amount;
+    private Date paymentDate;
+    private String paymentMethod;
+
+    public Payment(int paymentId, int bookingId, double amount, Date paymentDate, String paymentMethod) {
+        this.paymentId = paymentId;
+        this.bookingId = bookingId;
+        this.amount = amount;
+        this.paymentDate = paymentDate;
+        this.paymentMethod = paymentMethod;
+    }
+
+    public String toString() {
+    return String.format("Payment ID: %d, Booking: %d, Amount: RM%.2f", paymentId, bookingId, amount);    } 
 }
