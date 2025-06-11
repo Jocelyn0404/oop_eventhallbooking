b32400aa72c46112b149778970e0156f72e7cc47
Abhilashinie A/P Prabaharan <148563786+Abhilashinie@users.noreply.github.com>
2025-06-11 12:26:46 +0800

    Update Report.java

--- module3_abhi/Report.java
+++ module3_abhi/Report.java
@@ -1,5 +1,8 @@
-package module3_abhi;
-
 public class Report {
-    
+    private int reportId;
+    private Date generatedDate;
+    private String reportType;
+
+    public void generateBookingReport() {}
+    public void generateRevenueReport() {}
 }
