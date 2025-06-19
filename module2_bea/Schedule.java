e3d6bf2ba8a1626d24378e01b9ce79ff6196a36a
Beatrice Ann David <148671333+bettydavid@users.noreply.github.com>
2025-06-19 23:57:52 +0800

    Update Schedule.java

--- module2_bea/Schedule.java
+++ module2_bea/Schedule.java
@@ -4,8 +4,38 @@ class Schedule {
     private int hallId;
     private String bookingDate;
     private String timeSlot;
+    private static List<String> bookedSlots = new ArrayList<>();
 
-    public void addSlot() {}
-    public void removeSlot() {}
-    public boolean isSlotAvailable() { return true; }
+    public Schedule(int scheduleId, int hallId, String bookingDate, String timeSlot) {
+        this.scheduleId = scheduleId;
+        this.hallId = hallId;
+        this.bookingDate = bookingDate;
+        this.timeSlot = timeSlot;
+    }
+
+    public void addSlot() {
+        String slotKey = hallId + "-" + bookingDate + "-" + timeSlot;
+        if (!bookedSlots.contains(slotKey)) {
+            bookedSlots.add(slotKey);
+            System.out.println("Slot added: " + slotKey);
+        } else {
+            System.out.println("Slot already booked: " + slotKey);
+        }
+    }
+
+    public void removeSlot() {
+        String slotKey = hallId + "-" + bookingDate + "-" + timeSlot;
+        if (bookedSlots.contains(slotKey)) {
+            bookedSlots.remove(slotKey);
+            System.out.println("Slot removed: " + slotKey);
+        } else {
+            System.out.println("Slot not found: " + slotKey);
+        }
+    }
+
+    public boolean isSlotAvailable() {
+        String slotKey = hallId + "-" + bookingDate + "-" + timeSlot;
+        return !bookedSlots.contains(slotKey);
+    }
 }
+
