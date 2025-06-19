7ed5d30edd6c002fe3b46fcd4bcc7648da0cb444
Beatrice Ann David <148671333+bettydavid@users.noreply.github.com>
2025-06-19 23:08:10 +0800

    Update Notification.java

--- module2_bea/Notification.java
+++ module2_bea/Notification.java
@@ -5,9 +5,14 @@ class Notification {
     private String message;
     private Date date;
 
-    public void sendEmail() {}
-    public void sendSMS() {}
+    public Notification(int notificationId, int userId, String message) {
+        this.notificationId = notificationId;
+        this.userId = userId;
+        this.message = message;
+        this.date = new Date();
+    }
+
+    public void send() {
+        System.out.println("Notification sent to user " + userId + ": " + message);
+    }
 }
-
-
-
